2ad13d9fbe94f9f08c607e33c7a16b28cdddbfb3
Aries <aries@Mac.local>
2024-05-12 22:30:29 +0800

    fix console.log warning & home page layout issue

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(
+const root = document.getElementById('root')
+
+if (!root) {
+  throw new Error('Root element not found')
+}
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
     ,
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
